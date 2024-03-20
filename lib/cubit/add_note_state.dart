e180466a144317e7e25a08a2e67d9d72823bf006
HeshamAhmed1004 <heshamahmed1004@gmail.com>
2024-03-20 22:16:25 +0200

    add add note state

--- lib/cubit/add_note_state.dart
+++ lib/cubit/add_note_state.dart
@@ -2,3 +2,11 @@
 abstract class AddNoteStates{}
 
 class AddNoteInitial extends AddNoteStates{}
+class AddNoteLoading extends AddNoteStates{}
+class AddNoteSuccess extends AddNoteStates{}
+class AddNoteError extends AddNoteStates{
+  final String errorMessage;
+
+  AddNoteError(this.errorMessage);
+
+}
